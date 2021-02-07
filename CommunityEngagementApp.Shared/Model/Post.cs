73e375e577722ad893d19adf7881892e1b26188b
AsadDivvy <asad.rehman@divvy.com.au>
2021-02-07 22:11:00 +1100

    added models to be consumed by viewmodels

---
+++ CommunityEngagementApp.Shared/Model/Post.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CommunityEngagementApp.Shared.Model
+{
+    public sealed class Post
+    {
+        public Guid PostGUID { get; set; }
+        public string PostText { get; set; }
+        public bool IsLiked { get; set; }
+    }
+}
---
+++ CommunityEngagementApp.Shared/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommunityEngagementApp.Shared.Model
+{
+    public class User
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public Guid CouncilGuid { get; set; }
+        public bool IsConcilStaffMember { get; set; }
+
+        /// <summary>
+        /// Collection of 'UserDTO.Guid' this user is subcribed to
+        /// </summary>
+        public ISet<Guid> SubscribedTo { get; set; }
+
+        /// <summary>
+        /// Collection of 'PostDTO.Guid' this user likes
+        /// </summary>
+        public ISet<Guid> Likes { get; set; }
+    }
+}

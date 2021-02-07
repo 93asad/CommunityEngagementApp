ee175417b2a6032fa246404f5a23d4c29bc2e722
AsadDivvy <asad.rehman@divvy.com.au>
2021-02-08 01:04:40 +1100

    added view model and factory

---
+++ CommunityEngagementApp/ViewModel/PostsFragmentViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommunityEngagementApp.Shared.Data.Manager.Interfaces;
+using CommunityEngagementApp.Shared.Model;
+using CommunityEngagementApp.View.Adapters;
+
+namespace CommunityEngagementApp.ViewModel
+{
+    public class PostsFragmentViewModel : AndroidX.Lifecycle.ViewModel
+    {
+        public TaskCompletionSource<IList<Post>> PostsTcs { get; set; }
+        public IList<Post> Posts { get; set; }
+
+        IDataManager DataManager { get; }
+
+        object _lock = new object();
+        bool _isFetching;
+
+        public PostsFragmentViewModel(IDataManager dataManager)
+        {
+            DataManager = dataManager;
+        }
+
+        public bool ToggleLike(int itemPosition)
+        {
+            try
+            {
+                var post = Posts[itemPosition];
+                post.IsLiked = !post.IsLiked;
+
+                Task.Run(async delegate
+                {
+                    await DataManager.UpdateLikeForUserAsync(
+                        DataManager.SignedInUser.Guid,
+                        post.PostGUID,
+                        post.IsLiked);
+                });
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Task<IList<Post>> FetchPostsAsync()
+        {
+            lock (_lock)
+            {
+                if (_isFetching)
+                    return PostsTcs.Task;
+
+                _isFetching = true;
+            }
+
+            PostsTcs = new TaskCompletionSource<IList<Post>>();
+
+            return DataManager.GetPostsUsingCouncilAsync(
+                DataManager.SignedInUser.CouncilGuid).
+                ContinueWith(t =>
+                {
+                    lock (_lock)
+                    {
+                        PostsTcs = null;
+                        _isFetching = true;
+                    }
+
+                    if (t.Exception != null)
+                        return new List<Post>();
+                    else
+                        return t.Result;
+                });
+        }
+
+        internal Post GetPost(int itemPosition)
+        {
+            try
+            {
+                return Posts[itemPosition];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CommunityEngagementApp/ViewModel/PostsFragmentViewModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CommunityEngagementApp.Shared.Data.Manager.Interfaces;
+using CommunityEngagementApp.View.Adapters;
+using Java.Lang;
+
+namespace CommunityEngagementApp.ViewModel
+{
+    public class PostsFragmentViewModelFactory : Java.Lang.Object, AndroidX.Lifecycle.ViewModelProvider.IFactory
+    {
+        IDataManager _dataManager;
+
+        public PostsFragmentViewModelFactory()
+        {
+
+        }
+
+        public PostsFragmentViewModelFactory(IDataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        public Java.Lang.Object Create(Class p0)
+        {
+            return new PostsFragmentViewModel(_dataManager);
+        }
+    }
+}

e839a90ead35bf44e602a818e075fbabf66bfc90
AsadDivvy <asad.rehman@divvy.com.au>
2021-02-08 02:04:12 +1100

    added viewmodel, factory and layouts

---
+++ CommunityEngagementApp/ViewModel/CommentsFragmentViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommunityEngagementApp.Shared.Data.Manager.Interfaces;
+using CommunityEngagementApp.Shared.Model;
+
+namespace CommunityEngagementApp.ViewModel
+{
+    public class CommentsFragmentViewModel : AndroidX.Lifecycle.ViewModel
+    {
+        public Guid? PostGuid { get; set; }
+
+        public TaskCompletionSource<IList<Comment>> CommentsTcs { get; set; }
+        public IList<Comment> Comments { get; set; }
+
+        IDataManager DataManager { get; }
+
+        object _lock = new object();
+        bool _isFetching;
+
+        public CommentsFragmentViewModel(IDataManager dataManager)
+        {
+            DataManager = dataManager;
+        }
+
+        public Task<IList<Comment>> FetchCommentsAsync()
+        {
+            lock (_lock)
+            {
+                if (_isFetching)
+                    return CommentsTcs.Task;
+
+                _isFetching = true;
+            }
+
+            CommentsTcs = new TaskCompletionSource<IList<Comment>>();
+
+            return DataManager.GetCommentsAsync(PostGuid.Value).
+                ContinueWith(t =>
+                {
+                    lock (_lock)
+                    {
+                        CommentsTcs = null;
+                        _isFetching = false;
+                    }
+
+                    if (t.Exception != null)
+                        return new List<Comment>();
+                    else
+                        return t.Result;
+                });
+        }
+
+        public bool AddCommentAsync(string text)
+        {
+            try
+            {
+                var comment = Comment.Create(DataManager.SignedInUser.Guid, text);
+                Comments.Insert(0, comment);
+
+                Task.Run(async delegate
+                {
+                    await DataManager.AddCommentAsync(PostGuid.Value,
+                        Comment.Create(DataManager.SignedInUser.Guid, text));
+                });
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CommunityEngagementApp/ViewModel/CommentsFragmentViewModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CommunityEngagementApp.Shared.Data.Manager.Interfaces;
+using CommunityEngagementApp.View.Adapters;
+using Java.Lang;
+
+namespace CommunityEngagementApp.ViewModel
+{
+    public class CommentsFragmentViewModelFactory : Java.Lang.Object, AndroidX.Lifecycle.ViewModelProvider.IFactory
+    {
+        IDataManager _dataManager;
+
+        public CommentsFragmentViewModelFactory()
+        {
+
+        }
+
+        public CommentsFragmentViewModelFactory(IDataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        public Java.Lang.Object Create(Class p0)
+        {
+            return new CommentsFragmentViewModel(_dataManager);
+        }
+    }
+}

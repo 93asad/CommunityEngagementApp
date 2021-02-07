924cdf690ef825cf1a8549c8b5b23db045f0205d
AsadDivvy <asad.rehman@divvy.com.au>
2021-02-07 22:15:07 +1100

    add comment model to be consumed by ui

---
+++ CommunityEngagementApp.Shared/Model/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CommunityEngagementApp.Shared.Model
+{
+    public sealed class Comment
+    {
+        public Guid Guid { get; set; }
+        public Guid CommenterId { get; set; }
+        public string Text { get; set; }
+
+        internal static Comment Create(Guid commenterGuid, string comment)
+            => new Comment
+            {
+                CommenterId = commenterGuid,
+                Text = comment,
+                Guid = Guid.NewGuid()
+            };
+    }
+}

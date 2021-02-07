0d830287f7ef7820945a6839a6e48a5f5f597afe
AsadDivvy <asad.rehman@divvy.com.au>
2021-02-07 21:54:10 +1100

    basic structure

---
+++ CommunityEngagementApp/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using CommunityEngagementApp.Activity;
+using CommunityEngagementApp.Data.Source;
+using CommunityEngagementApp.Shared.Data.Manager;
+using CommunityEngagementApp.Shared.Data.Manager.Interfaces;
+using CommunityEngagementApp.Shared.Data.Source;
+using CommunityEngagementApp.Shared.Helper;
+
+namespace CommunityEngagementApp
+{
+    [Application]
+    public class App : Application
+    {
+        public static IDataManager DataManager { get; private set; }
+
+        public App(IntPtr handle, JniHandleOwnership ownerShip) : base(handle, ownerShip)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            SetupDataManager();
+        }
+
+        void SetupDataManager()
+        {
+#if MOCK
+            DataManager = new DataManager(new LocalSource(), new MockRemoteServer());
+#endif
+        }
+    }
+}
